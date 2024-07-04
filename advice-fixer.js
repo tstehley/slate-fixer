ae19d3aff9aad5b83b7cc53c1d8a7c1a104202b0
Talya Stehley <talya925@gmail.com>
2024-07-04 15:47:18 -0400

    Cleaned up a little to post as "version 0.1"

--- advice-fixer.js
+++ advice-fixer.js
@@ -8,13 +8,11 @@ async function follow_url(article_url){
         var column_title = await doc.getElementsByClassName("article__rubric").item(0).innerHTML.trim();
         var page_dateline = await doc.getElementsByClassName("article__dateline").item(0).innerText.trim();
         var date_chunks = page_dateline.split(" ");
-        //console.log(date_chunks)
         page_dateline = date_chunks[0]+" "+date_chunks[1]+" "+date_chunks[2].slice(0,-4)
-        //console.log(page_dateline)
     }catch(TypeError){
         //TODO Add Logic for Podcast Episodes
-        var column_title = "Podcast?";
-        var page_dateline = "IDK";
+        var column_title = "Podcast";
+        var page_dateline = "";
         console.log("I think this one's a podcast");
     }
     //console.log(page_dateline)
@@ -25,21 +23,25 @@ async function get_advice(links){
     for (let i=0; i<links.length; i++){
         //iterate through links
         //TODO Make this cleaner by using getElementsByClassName
-        //console.log(links.item(i))
-        if (links.item(i).className == "homepage-advice__side-by-side"){
-            console.log("found");
+        if (links.item(i).className == "homepage-advice__side-by-side" ||links.item(i).className == "homepage-advice__stack"){
             var article_link = links.item(i).href;
-            console.log(links.item(i));
             var article_data = await follow_url(article_link);
-            console.log(article_data);
             var new_title = article_data[0] + ": " + article_data[1];
             links.item(i).innerText = new_title;
-            //console.log(links.item(i))
         }
     }
 }
-document.body.style.border = "20px solid red";
+async function get_evergreen(document){
+    var evergreen = document.getElementsByClassName("homepage-advice__evergreen");
+    //console.log(evergreen.item(0).children.item(2).children)
+    for (let i=0; i<evergreen.item(0).children.item(2).children.length; i++){
+        console.log(evergreen.item(0).children.item(2).children.item(i));
+        var old_headline=evergreen.item(0).children.item(2).children.item(i).outerText;
+        var evergreen_json = evergreen.item(0).children.item(2).children.item(i).children.item(1).attributes.item(0).value;
+        await read_json(evergreen_json);
+    }
+}
 var all_links = document.links;
-//console.log("EEEEAAAAOOOOOUUUU")
 //TODO, add code to handle the big one in the middle and podcast episodes
 get_advice(all_links);
+get_evergreen(document);
